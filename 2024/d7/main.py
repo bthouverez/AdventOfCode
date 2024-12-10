406affdc125a7cf5a97cc11d6fbea85c1e9f524e
bthouverez <bastien.thouverez@gmail.com>
2024-12-10 16:35:07 +0100

    d7 unfinished

---
+++ 2024/d7/main.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+filename = 'input.txt'
+filename = 'test.txt'
+equations = []
+
+
+def init():
+    with open(filename) as file:
+        for line in file:
+            res, *numbers = [int(i) for i in line[:-1].replace(':', '').split()]
+            equations.append({res: numbers})
+
+
+def get_ops(size):
+    a = 0
+    res = []
+    while len(bin(a)) < size +3:
+        formatted = format(a, f'#0{size+2}b')[2:]
+        res.append(formatted.replace('0', '+').replace('1', '*'))
+        a += 1
+    return res
+
+
+def part_one():
+    init()
+    for eq in equations:
+        expected = list(eq.keys())[0]
+        print(expected, end=" → ")
+        print(eq[expected])
+        bins = get_ops(len(eq[expected]) - 1)
+        print(bins)
+
+        print()
+
+def part_two():
+    init()
+
+
+def main():
+    part_one()
+    # part_two()
+
+
+if __name__ == '__main__':
+    main()
